f49bd8d6326396c3afc2a9af1765a1af6dd7022e
Przemysław Kleist <pk0202@protonmail.com>
2022-10-27 12:53:14 +0200

    change database to postgres

--- subtilia/settings.py
+++ subtilia/settings.py
@@ -77,8 +77,14 @@ WSGI_APPLICATION = 'subtilia.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'subtila_database',
+        'USER': 'subtila_manager',
+        'PASSWORD': 'haslo',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
