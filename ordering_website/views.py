8b656ab6f4c81cb53d16389e3cba6b0266ba0a5f
Przemysław Kleist <pk0202@protonmail.com>
2022-11-09 21:05:46 +0100

    check if cart session exist before looping through cart session dictionary

--- ordering_website/views.py
+++ ordering_website/views.py
@@ -132,20 +132,21 @@ def cart_page(request):
 
     qty = 1
 
-    for id, qty in request.session["cart"].items():
-        if request.method == "POST":
-            qty = request.POST["product_" + str(id)]
+    if "cart" in request.session:
+        for id, qty in request.session["cart"].items():
+            if request.method == "POST":
+                qty = request.POST["product_" + str(id)]
 
-            dic = request.session["cart"]
-            dic[str(id)] = int(qty)
-            request.session["cart"] = dic
+                dic = request.session["cart"]
+                dic[str(id)] = int(qty)
+                request.session["cart"] = dic
 
-        cp = Wine.objects.get(pk=id)
-        price = int(qty) * float(cp.price)
-        whole_products.append([cp, qty, price])
-        sum_price += price
+            cp = Wine.objects.get(pk=id)
+            price = int(qty) * float(cp.price)
+            whole_products.append([cp, qty, price])
+            sum_price += price
 
-    total_price = float(sum_price) + 9.99
+    total_price = round(float(sum_price) + 9.99, 2)
 
     items_in_cart = get_cart_items_number(request)
 
