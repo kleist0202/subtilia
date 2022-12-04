66c696b31508ed6079917f5a06270b6c99eec2a7
Przemysław Kleist <pk0202@protonmail.com>
2022-12-04 15:38:45 +0100

    add some gaps between functions in ordering_website_extras

--- ordering_website/templatetags/ordering_website_extras.py
+++ ordering_website/templatetags/ordering_website_extras.py
@@ -3,20 +3,26 @@ from django.template.defaultfilters import stringfilter
 
 register = template.Library()
 
+
 @register.filter
 def addstr(arg1, arg2):
     """concatenate arg1 & arg2"""
     return str(arg1) + str(arg2)
 
+
 @register.filter
 @stringfilter
 def upto(value, delimiter=None):
     return value.split(delimiter)[0]
+
+
 upto.is_safe = True
+
 
 @register.filter
 def hash(h, key):
     return h[key]
+
 
 @register.filter
 def lookup(d, key):
